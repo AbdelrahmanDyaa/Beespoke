4dcf0bd6f258800b9139d619b9f80d13d8ff6859
AbdelrahmanDyaa <elnoby504@gmail.com>
2025-04-07 17:31:29 +0200

    Add @heroicons/react dependency and update About Us section with improved localization and layout

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import i18n from "./i18n"; // Import translation setup
 import App from "./App.jsx";
 import "./index.css";
 
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <I18nextProvider i18n={i18n}>
